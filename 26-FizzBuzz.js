c2fac41e4d70044d7cdc87667559a37b64837b6f
Satyaprasad7naik <naiksatyaprasad@gmail.com>
2026-01-25 16:35:02 +0530

    Add 26-FizzBuzz.js - Simple DSA Problem

---
+++ 26-FizzBuzz.js
@@ -0,0 +1,32 @@
+/**
+ * Problem 26: FizzBuzz
+ * 
+ * Given an integer n, return a string array answer (1-indexed) where:
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ * 
+ * Example:
+ * fizzBuzz(3) => ["1","2","Fizz"]
+ * fizzBuzz(5) => ["1","2","Fizz","4","Buzz"]
+ */
+
+function fizzBuzz(n) {
+    const result = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
+
+// Test cases
+console.log(fizzBuzz(15));
