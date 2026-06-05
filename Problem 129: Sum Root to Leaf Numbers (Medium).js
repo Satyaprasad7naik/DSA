108d9ae0a339f7ec6f0298b8684926553c3255d1
Satyaprasad7naik <naiksatyaprasad@gmail.com>
2026-06-05 21:44:46 +0530

    Create Problem 129: Sum Root to Leaf Numbers (Medium).js

---
+++ Problem 129: Sum Root to Leaf Numbers (Medium).js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function sumNumbers(root) {
+    // Edge case for empty tree
+    if (!root) return 0;
+
+    // Hint: A Depth-First Search (DFS) approach works great here.
+    // Think about how to pass the "current number" down to the child nodes.
+    
+};
