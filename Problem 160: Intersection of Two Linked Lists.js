c3171cde0786e12253b528ccebc84c8672df1a3e
Satyaprasad7naik <naiksatyaprasad@gmail.com>
2026-07-22 23:29:01 +0530

    Create Problem 160: Intersection of Two Linked Lists.js

---
+++ Problem 160: Intersection of Two Linked Lists.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null;
+    
+    let ptrA = headA;
+    let ptrB = headB;
+    
+    while (ptrA !== ptrB) {
+        ptrA = ptrA === null ? headB : ptrA.next;
+        ptrB = ptrB === null ? headA : ptrB.next;
+    }
+    
+    return ptrA;
+};
